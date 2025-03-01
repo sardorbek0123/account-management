2768ed623245cc7764c059571b90327764dd95ed
Sardorbek <jabborovsardor21@gmail.com>
2025-03-01 13:12:47 +0500

    feat: header added

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,13 @@
 <template>
-  <main>
-
+  <main class="max-w-7xl mx-auto p-8">
+    <div class="flex justify-between items-center mb-4">
+      <h1 class="text-2xl font-bold">Управление учетными записями</h1>
+      <button class="p-2 border border-gray-400 rounded-md ">
+        <Plus class="w-4 h-4" />
+      </button>
+    </div>
   </main>
 </template>
 <script setup lang="ts">
+import Plus from "@/components/Icons/Plus.vue";
 </script>
