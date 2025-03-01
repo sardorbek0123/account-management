8c2e03359483645f07f35082ab651d72b0f4aa4d
Sardorbek <jabborovsardor21@gmail.com>
2025-03-01 16:20:19 +0500

    feat: password toggle

--- src/components/Input/Text.vue
+++ src/components/Input/Text.vue
@@ -1,19 +1,32 @@
 <template>
   <div class="flex flex-col w-full relative">
-    <input
-        :type="type"
-        :value="inputValue"
-        @input="updateValue"
-        @blur="handleBlur"
-        :placeholder="placeholder"
-        :class="['w-full px-3 py-2 border rounded-lg focus:outline-none', error ? 'border-red-500' : 'border-gray-300']"
-    />
+    <div class="relative w-full">
+      <input
+          :type="isPasswordVisible ? 'text' : type"
+          :value="inputValue"
+          @input="updateValue"
+          @blur="handleBlur"
+          :placeholder="placeholder"
+          :class="['w-full px-3 py-2 border rounded-lg focus:outline-none', error ? 'border-red-500' : 'border-gray-300']"
+      />
+      <button
+          v-if="type === 'password'"
+          type="button"
+          @click="togglePasswordVisibility"
+          class="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-gray-700"
+      >
+        <EyeIcon v-if="!isPasswordVisible" class="w-5 h-5"/>
+        <EyeHideIcon v-else class="w-5 h-5"/>
+      </button>
+    </div>
     <small v-if="error" class="text-red-500 text-xs mt-1 absolute top-9">{{ error }}</small>
   </div>
 </template>
 
 <script setup>
 import { ref, watch } from "vue";
+import EyeIcon from "@/components/Icons/Eye.vue";
+import EyeHideIcon from "@/components/Icons/EyeHide.vue";
 
 const props = defineProps({
   modelValue: String,
@@ -28,6 +41,7 @@ const props = defineProps({
 const emit = defineEmits(["update:modelValue", "blur"]);
 
 const inputValue = ref(props.modelValue);
+const isPasswordVisible = ref(false);
 
 watch(
     () => props.modelValue,
@@ -42,5 +56,9 @@ const updateValue = (event) => {
 
 const handleBlur = (event) => {
   emit("blur", event);
+};
+
+const togglePasswordVisibility = () => {
+  isPasswordVisible.value = !isPasswordVisible.value;
 };
 </script>
